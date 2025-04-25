f50ca83c7a91a0727759c84185e0130378dc3422
JereHutch <jeremiahhutchison02@gmail.com>
2025-04-25 02:10:53 -0500

    Update ocp.py

--- ocp.py
+++ ocp.py
@@ -11,8 +11,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-#Let's Show the TITLE!!
+#Let's Show the TITLE and LOGO!
 st.set_page_config(page_title="Contract Overlap Analyzer", layout="wide")
+st.image("BongoLogo.PNG", width=150) 
 st.title("Overlapinator: Contract Overlap Analyzer")
 st.write("Welcome! To Begin, Upload an Excel file with your contract data.")
 st.write("Required Columns: Contract_Name, Contract_ID, Service_Type, Start_Date, End_Date, Cost, Maintenance")
